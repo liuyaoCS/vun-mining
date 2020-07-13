7c8876a748d0a4afb460a99710290cc174f46818
liuyao-s <liuyao-s@360.cn>
2020-07-13 10:42:44 +0800

    update readme

--- test/test.c
+++ test/test.c
@@ -1,10 +1,16 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <unistd.h>
 
 int main(int argc,char* argv[]){
-    int i=1;
-    int j=2;
+    // int i=1;
+    // int j=2;
 
-    printf("i=%d j=%d\n", i,j);
+    // printf("i=%d j=%d\n", i,j);
+    while(1){
+        printf("sleeping...\n");
+        sleep(1);
+
+    }
 }
